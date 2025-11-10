51124336fa3e22ff93b877471d104d507275da9e
Changhua <lichanghua0821@gmail.com>
2025-11-11 07:57:56 +0800

    feat: add icon

--- crates/serwave-app/build.rs
+++ crates/serwave-app/build.rs
@@ -1,3 +1,10 @@
 fn main() {
     slint_build::compile("src/ui.slint").unwrap();
+
+    #[cfg(windows)]
+    {
+        let mut res = winres::WindowsResource::new();
+        res.set_icon("../../assets/icon.ico");
+        res.compile().unwrap();
+    }
 }
